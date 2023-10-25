070db22cd87a6d5e91630b1098e9089f1e911942
TJ1B <149000558+TJ1B@users.noreply.github.com>
2023-10-25 18:27:57 -0400

    rename styles

--- scripts.js
+++ scripts.js
@@ -1,11 +1,7 @@
 let songs = [];
 
 // Fetch the songs from the JSON file
-fetch('songs.json', {
-    headers : { 
-        'Content-Type': 'application/json; charset=UTF-8'
-    }
-})
+fetch('songs.json')
     .then(response => response.json())
     .then(data => {
         songs = data;
@@ -13,19 +9,22 @@ fetch('songs.json', {
     });
 
 function renderSongs(filteredSongs) {
+    const songList = document.getElementById('songList');
     songList.innerHTML = '';
+
     for (const item of filteredSongs) {
         const li = document.createElement('li');
-        li.textContent = `${item.song} by ${item.singer} [${item.language}]`;
+        li.textContent = `${item.songName} 由 ${item.singerName} [${item.language}]`;
         songList.appendChild(li);
     }
 }
 
+const searchBox = document.getElementById('searchBox');
 searchBox.addEventListener('input', function() {
     const query = searchBox.value.toLowerCase();
-    const filteredSongs = songs.filter(item => 
-        item.song.toLowerCase().includes(query) || 
-        item.singer.toLowerCase().includes(query) ||
+    const filteredSongs = songs.filter(item =>
+        item.songName.toLowerCase().includes(query) ||
+        item.singerName.toLowerCase().includes(query) ||
         item.language.toLowerCase().includes(query)
     );
     renderSongs(filteredSongs);
