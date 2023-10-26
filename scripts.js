d02d87a56868b3c498ccbc9cbc75353034625475
TJ1B <149000558+TJ1B@users.noreply.github.com>
2023-10-25 22:17:23 -0400

    draft version 10.25 Random Scroll

--- scripts.js
+++ scripts.js
@@ -1,31 +1,79 @@
 let songs = [];
 
-// Fetch the songs from the JSON file
-fetch('songs.json')
-    .then(response => response.json())
-    .then(data => {
-        songs = data;
-        renderSongs(songs); // Render songs after fetching
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const response = await fetch('https://tj1b.github.io/musicbox/songs.json');
+        songs = await response.json();
+        renderSongs(songs);
+    } catch (error) {
+        console.error('Error fetching the song list:', error);
+    }
+});
+
+document.getElementById('searchBox').addEventListener('input', function() {
+    const searchTerm = this.value.toLowerCase();
+    const filteredSongs = songs.filter(song => 
+        song.songName.toLowerCase().includes(searchTerm) || 
+        song.singerName.toLowerCase().includes(searchTerm) || 
+        song.language.toLowerCase().includes(searchTerm)
+    );
+    renderSongs(filteredSongs);
+});
+
+let currentSort = { key: 'songName', direction: 'asc' };
+document.querySelectorAll('#songTable th').forEach(th => {
+    th.addEventListener('click', function() {
+        const sortKey = this.dataset.sort;
+        if (sortKey) {
+            currentSort.direction = (currentSort.key === sortKey && currentSort.direction === 'asc') ? 'desc' : 'asc';
+            currentSort.key = sortKey;
+            renderSongs(songs);
+        }
     });
+});
 
 function renderSongs(filteredSongs) {
+    if (!Array.isArray(filteredSongs)) {
+        console.error("Expected an array but got:", filteredSongs);
+        return;
+    }
+
     const songList = document.getElementById('songList');
     songList.innerHTML = '';
 
+    filteredSongs.sort((a, b) => {
+        if (a[currentSort.key] < b[currentSort.key]) return currentSort.direction === 'asc' ? -1 : 1;
+        if (a[currentSort.key] > b[currentSort.key]) return currentSort.direction === 'asc' ? 1 : -1;
+        return 0;
+    });
+
     for (const item of filteredSongs) {
-        const li = document.createElement('li');
-        li.textContent = `${item.songName} 由 ${item.singerName} [${item.language}]`;
-        songList.appendChild(li);
+        const row = songList.insertRow();
+        row.insertCell().innerText = item.songName;
+        row.insertCell().innerText = item.singerName;
+        row.insertCell().innerText = item.language;
     }
 }
 
-const searchBox = document.getElementById('searchBox');
-searchBox.addEventListener('input', function() {
-    const query = searchBox.value.toLowerCase();
-    const filteredSongs = songs.filter(item =>
-        item.songName.toLowerCase().includes(query) ||
-        item.singerName.toLowerCase().includes(query) ||
-        item.language.toLowerCase().includes(query)
-    );
-    renderSongs(filteredSongs);
+document.getElementById('randomPick').addEventListener('click', function() {
+    if (songs.length === 0) {
+        document.getElementById('randomResult').innerText = "歌曲列表为空！";
+        return;
+    }
+
+    const randomSong = songs[Math.floor(Math.random() * songs.length)];
+
+    // Highlight the song in the table
+    document.querySelectorAll('#songList tr').forEach((row, index) => {
+        if (songs[index] === randomSong) {
+            row.classList.add('highlighted-row');
+
+            // Scroll to the highlighted song
+            row.scrollIntoView({ block: 'center', behavior: 'smooth' });
+        } else {
+            row.classList.remove('highlighted-row');
+        }
+    });
+
+    document.getElementById('randomResult').innerText = `随机歌曲：${randomSong.songName} 由 ${randomSong.singerName}`;
 });
